fccdc2f54e2929fcf6b57b573a3e79e4d19b2189
ArtycoBW <artyco.bw@gmail.com>
2023-12-07 14:34:39 +0300

    dice realization

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import "./Dise.css";
+
+const SIDES = [
+  { x: 0, y: 0 },
+  { x: 90, y: 180 },
+  { x: 0, y: 90 },
+  { x: 0, y: 270 },
+  { x: 90, y: 0 },
+  { x: 360, y: 180 },
+];
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [roll, setRoll] = useState(0);
+  const [isRolling, setIsRolling] = useState(false);
+
+  const handleClick = () => {
+    setIsRolling(true);
+    setRoll(Math.floor(Math.random() * SIDES.length));
+    setTimeout(() => {
+      setIsRolling(false);
+    }, 1000);
+  };
+
+  return (
+    <div className="roll">
+      <div
+        className="cube"
+        style={{
+          transform: `rotateY(${SIDES[roll].y}deg) rotateX(${SIDES[roll].x}deg)`,
+        }}
+      >
+        {SIDES.map((_, index) => (
+          <div key={index} className={`side side-${index + 1}`}></div>
+        ))}
+      </div>
+      <button
+        className="roll_btn"
+        onClick={handleClick}
+        type="button"
+        disabled={isRolling}
+      >
+        Крутить
+      </button>
+    </div>
+  );
 };
